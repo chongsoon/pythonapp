f6b2176eebaca422d9b8a6f2f5e592e92bbd6abc
chongsoon <chongsoon@ntu.edu.sg>
2018-04-16 15:06:44 +0800

    change main

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello testing testing")
+import requests
+import json
+import random
+import string
+
+url = "https://3auvqg06yk.execute-api.ap-southeast-1.amazonaws.com/test/student"
+
+randomString = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(8))
+
+
+data = {'name': 'student-'+randomString,
+        'description':'description-'+randomString}
+
+dataJSON = json.dumps(data).encode('utf8')
+
+r = requests.post(url=url, data = dataJSON)
+
+print(r.text)
